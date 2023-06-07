06d92f4a6c9fff8574c891400a46903e6e9cc94e
necobra <necobrane105@gmail.com>
2023-06-07 20:22:08 +0300

    test2

--- script.js
+++ script.js
@@ -1,121 +1,156 @@
-// Отримуємо необхідні елементи з DOM
-const productNameInput = document.querySelector('.product-search input');
-const addButton = document.querySelector('.add');
-const productsList = document.querySelector('.products');
+function drawProduct(name, amount, isBought) {
+    drawProductPanel(name, amount, isBought);
+    drawProductCard(name, amount, isBought);
+}
 
-// Функція для додавання товару до списку
-function addProduct() {
-    // Отримуємо назву товару з поля вводу
-    const productName = productNameInput.value;
+function drawProductPanel(name, amount, isBought) {
+    // Ліва нанель
+    const productContainer = document.createElement('div');
+    productContainer.classList.add('inline-container');
 
-    if (productName) { // Перевіряємо, чи не порожнє поле вводу
-        // Створюємо елементи для нового товару
-        const productContainer = document.createElement('div');
-        productContainer.classList.add('inline-container');
+    const product = document.createElement('div');
+    product.classList.add('product');
 
-        const product = document.createElement('div');
-        product.classList.add('product');
+    const productNameElement = document.createElement('div');
+    productNameElement.classList.add('product-name');
+    productNameElement.textContent = name;
+    if (isBought) productNameElement.style.textDecoration = 'line-through';
+    productNameElement.addEventListener('click', editProductName);
 
-        const productNameElement = document.createElement('div');
-        productNameElement.classList.add('product-name');
-        productNameElement.textContent = productName;
+    const amountDiv = document.createElement('div');
+    amountDiv.classList.add('amount');
 
-        const amount = document.createElement('div');
-        amount.classList.add('amount');
+    const minusDiv = document.createElement('div');
+    if (isBought) minusDiv.id = 'invisible';
 
-        const minusDiv = document.createElement('div');
-
-        const minusButton = document.createElement('button');
+    const minusButton = document.createElement('button');
+    if (amount != 1) {
         minusButton.classList.add('minus');
-        minusButton.setAttribute('data-tooltip', 'Відняти');
-        minusButton.textContent = '-';
+    }
+    else {
+        minusButton.classList.add('minus-unactive');
+    }
 
-        const productAmount = document.createElement('div');
-        productAmount.classList.add('product-amount');
-        productAmount.textContent = '1';
+    minusButton.setAttribute('data-tooltip', 'Відняти');
+    minusButton.textContent = '-';
+    minusButton.addEventListener('click', editProductQuantity)
 
-        const plusDiv = document.createElement('div');
+    const productAmount = document.createElement('div');
+    productAmount.classList.add('product-amount');
+    productAmount.textContent = amount;
 
-        const plusButton = document.createElement('button');
-        plusButton.classList.add('plus');
-        plusButton.setAttribute('data-tooltip', 'Додати');
-        plusButton.textContent = '+';
+    const plusDiv = document.createElement('div');
+    if (isBought) plusDiv.id = 'invisible';
 
-        const acts = document.createElement('div');
-        acts.classList.add('acts');
+    const plusButton = document.createElement('button');
+    plusButton.classList.add('plus');
+    plusButton.setAttribute('data-tooltip', 'Додати');
+    plusButton.textContent = '+';
+    plusButton.addEventListener('click', editProductQuantity);
 
-        const boughtButton = document.createElement('button');
-        boughtButton.classList.add('btn-bought');
-        boughtButton.setAttribute('data-tooltip', 'Куплено');
-        boughtButton.textContent = 'Куплено';
-        boughtButton.addEventListener('click', markProductAsUnbought);
+    const acts = document.createElement('div');
+    acts.classList.add('acts');
 
-        const deleteButton = document.createElement('button');
-        deleteButton.classList.add('btn-x');
-        deleteButton.setAttribute('data-tooltip', 'Видалити');
-        deleteButton.textContent = 'x';
-        deleteButton.addEventListener('click', deleteProduct);
+    const boughtButton = document.createElement('button');
+    let className;
+    if (isBought) className = 'btn-no-bought';
+    else className = 'btn-bought';
+    boughtButton.classList.add(className);
+    let tooltipText;
+    if (isBought) tooltipText = 'Не куплено';
+    else tooltipText = 'Куплено';
+    boughtButton.setAttribute('data-tooltip', tooltipText);
+    let text;
+    if (isBought) text = 'Не куплено';
+    else text = 'Куплено';
+    boughtButton.textContent = text;
+    let func;
+    if (isBought) func = markProductAsUnbought;
+    else func = markProductAsBought;
+    boughtButton.addEventListener('click', func);
 
-        // Додаємо елементи до DOM
-        minusDiv.appendChild(minusButton);
-        plusDiv.appendChild(plusButton);
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('btn-x');
+    deleteButton.setAttribute('data-tooltip', 'Видалити');
+    deleteButton.textContent = 'x';
+    deleteButton.addEventListener('click', deleteProduct);
+    if (isBought) deleteButton.id = 'none';
 
-        amount.appendChild(minusDiv);
-        amount.appendChild(productAmount);
-        amount.appendChild(plusDiv);
+    // Додаємо елементи до DOM
+    minusDiv.appendChild(minusButton);
+    plusDiv.appendChild(plusButton);
 
-        acts.appendChild(boughtButton);
-        acts.appendChild(deleteButton);
+    amountDiv.appendChild(minusDiv);
+    amountDiv.appendChild(productAmount);
+    amountDiv.appendChild(plusDiv);
 
-        product.appendChild(productNameElement);
-        product.appendChild(amount);
-        product.appendChild(acts);
+    acts.appendChild(boughtButton);
+    acts.appendChild(deleteButton);
 
-        productContainer.appendChild(product);
-        productsList.appendChild(productContainer);
+    product.appendChild(productNameElement);
+    product.appendChild(amountDiv);
+    product.appendChild(acts);
 
-        // Очищуємо поле вводу
-        productNameInput.value = '';
+    productContainer.appendChild(product);
+    document.querySelector('.products').appendChild(productContainer);
 
-        // Перенаправляємо фокус на поле вводу
-        productNameInput.focus();
+
+    // Права панель
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+    productCard.id = name;
+    productCard.textContent = name;
+    const productCardAmount = document.createElement('div');
+    productCardAmount.classList.add('product-card-amount')
+    productCardAmount.textContent = amount;
+
+    productCard.appendChild(productCardAmount);
+
+    if (isBought) {
+        productCard.style.textDecoration = 'line-through';
+        document.querySelector('.product-list-bought').appendChild(productCard);
+    }
+    else {
+        document.querySelector('.product-list-remainder').appendChild(productCard);
     }
 }
 
-// Налаштування функціональності кнопки Додати
+function drawProductCard(name, amount, isBought) {
+
+}
+
+
+// Додати
+const addButton = document.querySelector('.add');
+function addProduct(event) {
+    const productNameInput = document.querySelector('.product-search input');
+    const productName = productNameInput.value;
+    if (productName) {
+        // Якщо ім'я коректне
+        // Зводим ім'я до стандартного вигляду
+        const correctName = productName[0].toUpperCase() + productName.substr(1).toLowerCase();
+        const productCard = document.getElementById(correctName);
+        if (!productCard) {
+            // Якщо таке ім'я ще не існує
+            
+            drawProduct(correctName, 1, false);
+            // Очищуємо поле вводу
+            productNameInput.value = '';
+        }
+    }
+    // Перенаправляємо фокус на поле вводу
+    productNameInput.focus();
+}
 addButton.addEventListener('click', addProduct);
-
-
-// Функція для видалення товару зі списку
-function deleteProduct(event) {
-    const product = event.target.closest('.inline-container');
-
-    if (product) {
-        product.remove();
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        addProduct();
     }
-}
+})
 
-//
-// кнопка Видалити
-function deleteProduct(event) {
-    const product = event.closest('.product');
-    const isBought = product.classList.contains('bought');
-
-    if (!isBought) {
-        deleteProduct(event);
-    }
-
-}
-
-
-
-//
-// Куплено/НеКуплено
-const boughtButtons = document.querySelectorAll('.btn-bought');
-const noBoughtButtons = document.querySelectorAll('.btn-no-bought');
 
 // Функція для відмітки товару як купленого
-function markProductAsBought(event) {
+function markProductAsUnbought(event) {
     const product = event.target.closest('.product');
 
     product.classList.remove('bought');
@@ -136,12 +171,19 @@ function markProductAsBought(event) {
     const deleteButton = product.querySelector('.btn-x');
     deleteButton.style.display = 'block';
 
-    editButton.removeEventListener('click', markProductAsBought);
-    editButton.addEventListener('click', markProductAsUnbought);
+    editButton.removeEventListener('click', markProductAsUnbought);
+    editButton.addEventListener('click', markProductAsBought);
+
+    // права панель
+    //console.log(name.textContent);
+    const productCard = document.getElementById(name.textContent);
+    productCard.remove();
+    document.querySelector('.product-list-remainder').appendChild(productCard);
+    productCard.style.textDecoration = 'none';
 }
 
 // Функція для відміни відмітки товару як НЕ купленого
-function markProductAsUnbought(event) {
+function markProductAsBought(event) {
     const product = event.target.closest('.product');
 
     product.classList.add('bought');
@@ -162,18 +204,154 @@ function markProductAsUnbought(event) {
     const deleteButton = product.querySelector('.btn-x');
     deleteButton.style.display = 'none';
 
-    editButton.removeEventListener('click', markProductAsUnbought);
-    editButton.addEventListener('click', markProductAsBought);
+    editButton.removeEventListener('click', markProductAsBought);
+    editButton.addEventListener('click', markProductAsUnbought);
+
+    // права панель
+    //console.log(name.textContent);
+    const productCard = document.getElementById(name.textContent);
+    productCard.remove();
+    document.querySelector('.product-list-bought').appendChild(productCard);
+    productCard.style.textDecoration = 'line-through';
+}
+
+// Функція для видалення товару зі списку
+function deleteProduct(event) {
+    const product = event.target.closest('.inline-container');
+    product.remove();
+    // права панель
+    //console.log(name.textContent);
+    const name = product.querySelector('.product-name');
+    const productCard = document.getElementById(name.textContent);
+    productCard.remove();
+}
+
+// Функція для редагування назви товару
+function editProductName(event) {
+    const product = event.target.closest('.product');
+    const nameElement = product.querySelector('.product-name');
+    const inputElement = document.createElement('input');
+    const nameStr = nameElement.textContent
+    inputElement.value = nameStr;
+    inputElement.classList.add('editing');
+
+    nameElement.textContent = null;
+    nameElement.appendChild(inputElement);
+    inputElement.focus();
+
+    inputElement.addEventListener('blur', () => {
+        let newNameStr = inputElement.value;
+        let correctNewNameStr;
+        console.log(newNameStr);
+        if (!newNameStr) {
+            correctNewNameStr = nameStr[0];
+        }
+        else {
+            correctNewNameStr = newNameStr[0].toUpperCase() + newNameStr.substr(1).toLowerCase();
+        }
+
+        const productCard = document.getElementById(nameStr);
+        const amountDiv = productCard.getElementsByTagName('div')[0];
+        productCard.textContent = correctNewNameStr;
+        productCard.appendChild(amountDiv);
+        productCard.id = correctNewNameStr;
+
+        nameElement.textContent = correctNewNameStr;
+        inputElement.remove();
+    });
+}
+
+// Функція для редагування кількості товару
+function editProductQuantity(event) {
+    const product = event.target.closest('.product');
+    const countElement = product.querySelector('.product-amount');
+    const minusButton = product.querySelector('.minus');
+    const plusButton = product.querySelector('.plus');
+    let count = parseInt(countElement.textContent);
+
+    if (event.target.classList.contains('minus')) {
+        count--;
+        if (count === 1) {
+            minusButton.classList.remove('minus');
+            minusButton.classList.add('minus-unactive');
+        }
+    } else if (event.target.classList.contains('plus')) {
+        count++;
+        const unactiveMinusButoon = product.querySelector('.minus-unactive');
+        if (count === 2) {
+            unactiveMinusButoon.classList.remove('minus-unactive');
+            unactiveMinusButoon.classList.add('minus');
+        }
+    }
+
+    countElement.textContent = count;
+
+    // права панель
+    const name = product.querySelector('.product-name');
+    const productCard = document.getElementById(name.textContent);
+    const amountDiv = productCard.getElementsByTagName('div')[0];
+    amountDiv.textContent = count;
 }
 
 
+// Функція для оновлення статистики
+function updateStatistics() {
+    const toBuyList = document.querySelector('.to-buy-list');
+    const purchasedList = document.querySelector('.purchased-list');
+    let toBuyHTML = '';
+    let purchasedHTML = '';
 
-// Налаштування функціональності кнопки "Куплено"
-for (let button of boughtButtons) {
-    button.addEventListener('click', markProductAsUnbought);
+    // Очистка списків
+    toBuyList.innerHTML = '';
+    purchasedList.innerHTML = '';
+
+    // Отримання всіх товарів
+    const products = document.querySelectorAll('.product');
+
+    // Перебір товарів
+    products.forEach((product) => {
+        const name = product.querySelector('.name').textContent;
+        const count = parseInt(product.querySelector('.count').textContent);
+        const isPurchased = product.classList.contains('purchased');
+
+        const listItem = `<li>${name} (${count})</li>`;
+
+        if (isPurchased) {
+            purchasedHTML += listItem;
+        } else {
+            toBuyHTML += listItem;
+        }
+    });
+
+    // Відображення статистики
+    toBuyList.innerHTML = toBuyHTML;
+    purchasedList.innerHTML = purchasedHTML;
 }
 
-// Налаштування функціональності кнопки "Не куплено"
-for (let button of noBoughtButtons) {
-    button.addEventListener('click', markProductAsBought);
+// ресет даних
+function reset(){
+    let productArray = [['Помідори', 2, true], ['Печиво', 2, false], ['Сир', 1, false]]
+
+    for (let product of productArray) {
+        drawProduct(product[0], product[1], product[2]);
+    }
+    localStorage.setItem("array", productArray);
 }
+
+// загрузка старих данних
+function loading(){
+    let productArray = localStorage.getItem("array").split(',');
+    //console.log(productArray);
+    let i = 0;
+    while (i < productArray.length){
+        drawProduct(productArray[i],productArray[i+1],productArray[i+2]==='true'?true:false)
+        i+=3;
+    }
+}
+
+function resetLocalStorage(){
+    localStorage.removeItem('array');
+}
+
+let productsDictionaty;
+loading();
